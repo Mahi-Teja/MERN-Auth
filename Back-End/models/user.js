a6940abfe8698735cc2dd74d298afad335a8697e
mahineeli <mahineeli899@gmail.com>
2024-08-07 22:24:13 +0530

    created Banckend for signup and login (post)routes

---
+++ Back-End/models/user.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const UserSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    roles: {
+      type: Object,
+      default: {
+        User: 1999,
+      },
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
